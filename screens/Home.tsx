ce1187673179c351fe92af69502bcc1fb31ea43c
mxdi9i7 <peter.zheng88228@gmail.com>
2021-06-12 17:01:24 -0400

    updates

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -2,6 +2,7 @@ import { useNavigation } from '@react-navigation/core';
 import React, { useEffect, useState } from 'react';
 import {
   Button,
+  Image,
   StyleSheet,
   Text,
   Touchable,
@@ -11,13 +12,23 @@ import {
 } from 'react-native';
 import { fetchAllPosts } from '../services/posts';
 import globalStyles from '../styles/global';
+import moment from 'moment';
 
 interface Props {}
 
+export interface User {
+  email: string;
+  phone: string;
+  username: string;
+  avatar: string;
+  gender: number;
+}
+
 export interface Post {
   title: string;
-  author: string;
+  author: User;
   _id: string;
+  createdAt: string;
 }
 
 const HomeScreen = (props: Props) => {
@@ -48,7 +59,20 @@ const HomeScreen = (props: Props) => {
           style={[globalStyles.contentContainer]}
           key={index}
         >
-          <Text style={styles.author}>Author: {value.author}</Text>
+          <View style={styles.headerContainer}>
+            <View style={styles.authorContainer}>
+              <Image
+                style={styles.avatar}
+                source={{ uri: value.author.avatar }}
+              />
+              <Text style={styles.author}>{value.author.username}</Text>
+            </View>
+            <View style={styles.timestampContainer}>
+              <Text style={styles.timestamp}>
+                {moment(value.createdAt).fromNow()}
+              </Text>
+            </View>
+          </View>
           <Text style={styles.body}>{value.title}</Text>
         </TouchableOpacity>
       ))}
@@ -61,12 +85,32 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 5,
   },
+  timestampContainer: {},
+  timestamp: {
+    fontSize: 10,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
   author: {
-    fontSize: 12,
+    fontSize: 14,
+    fontWeight: '600',
+    marginLeft: 8,
+  },
+  authorContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
   body: {
     fontSize: 16,
     marginTop: 10,
+  },
+  avatar: {
+    height: 20,
+    width: 20,
+    borderRadius: 15,
   },
 });
 
---
+++ screens/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+interface Props {}
+
+const LoginScreen = (props: Props) => {
+  return (
+    <div>
+      <Text>Login</Text>
+    </div>
+  );
+};
+
+export default LoginScreen;
